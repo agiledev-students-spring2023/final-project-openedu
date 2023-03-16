24ccefd34cba415808518ba43a906d7324a3af3a
Frank_Liu <lxteng2016@126.com>
2023-03-16 14:08:53 -0400

    feat:change the basic layout of the begin page; add a front cover. (#43)

--- front-end/src/pages/Index/index.jsx
+++ front-end/src/pages/Index/index.jsx
@@ -1,43 +1,115 @@
-import React from 'react';
+import React from "react";
 
-import { Typography, Box, Container } from '@mui/material';
+import { Typography, Box, Button } from "@mui/material";
 
-// This is an example page, called Index 
 export default function Index() {
-
+  function handleClick() {
+    
+  }
   return (
     <div>
-      <Box sx={{
-        height: { xs: 350, md: 400, lg: 600 },
-      }}>
-
-        <Typography sx={{
-          paddingTop: '100px',
-          fontSize: { xs: '50px', md: '100px', lg: '120px' },
-          fontWeight: 'bold',
-          color: 'Blue'
-        }}>
-          Start Here
+      <Box
+        sx={{
+          position: "absolute",
+          width: "251px",
+          height: "79px",
+          top: "219px",
+          left: "28px",
+          textAlign: "center",
+        }}
+        backgroundColor="#D9D9D9"
+      >
+        <Typography
+          sx={{
+            margin: "auto",
+            fontFamily: "Raleway",
+            fontStyle: "normal",
+            fontWeight: "800",
+            fontSize: "50px",
+            lineHeight: "59px",
+          }}
+        >
+          LOGO
         </Typography>
-
       </Box>
-      <Container maxWidth='lg'>
-        <Box sx={{
-          paddingTop: '100px',
-          marginBottom: '200px'
-        }}>
-          <Typography sx={{
-            paddingTop: '10px',
-            fontSize: { xs: '15px', md: '25px', lg: '32px' },
-            fontWeight: 'bold',
-            color: 'black',
-            float: 'left'
-          }}>
-            Guess You Like...
-          </Typography>
-        </Box>
-      </Container>
+      <Typography
+        sx={{
+          position: "absolute",
+          width: "58px",
+          height: "32px",
+          left: "28px",
+          top: "356px",
 
+          fontFamily: "Arimo",
+          fontStyle: "normal",
+          fontWeight: "400",
+          fontSize: "25px",
+          lineHeight: "29px",
+          textAlign: "left",
+        }}
+      >
+        The
+      </Typography>
+
+      <Typography
+        sx={{
+          position: "absolute",
+          width: "251px",
+          height: "54px",
+          left: "28px",
+          top: "388px",
+
+          fontFamily: "Arimo",
+          fontStyle: "normal",
+          fontWeight: "700",
+          fontSize: "47px",
+          lineHeight: "57px",
+          textAlign: "left",
+        }}
+      >
+        All-in-one
+      </Typography>
+      <Typography
+        sx={{
+          position: "absolute",
+          width: "266px",
+          height: "78px",
+          left: "28px",
+          top: "448px",
+
+          fontFamily: "Arimo",
+          fontStyle: "normal",
+          fontWeight: "400",
+          fontSize: "24px",
+          lineHeight: "28px",
+          textAlign: "left",
+        }}
+      >
+        Computer Science Learning Platform
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "0px",
+          gap: "8px",
+
+          position: "absolute",
+          width: "143px",
+          height: "48px",
+          left: "28px",
+          top: "532px",
+
+          background: "#FFFFFF",
+          border: "2px solid #000000",
+        }}
+      >
+        <Button variant="text" onClick={handleClick} sx={{color:'black'}}>
+          Get Started
+        </Button>
+      </Box>
     </div>
   );
 }

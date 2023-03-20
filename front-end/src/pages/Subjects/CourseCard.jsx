29d0464497d70ad8e1b1cd224acd8c668a513f60
TY37Zhang <tz1308@nyu.edu>
2023-03-19 23:02:34 -0400

    <style>: update SubjectCard, CourseCard styling

--- front-end/src/pages/Subjects/CourseCard.jsx
+++ front-end/src/pages/Subjects/CourseCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {Button, Card, CardContent, Typography} from "@mui/material";
+import {Button, Card, Grid,CardContent, Typography} from "@mui/material";
+import * as Constants from "../../util/Constants.mjs";
 
 export function CourseCard(
     /** @type {{ entry: { id: number; name: string; description: string; completionRate: number; } }} */
@@ -11,25 +12,29 @@ export function CourseCard(
         navigate("../course/CourseDetail/${entry.id}");
     };
     return (
-        <Card
+        <Grid
             sx={{
                 minWidth: 200,
-            }}
-        >
-            <CardContent>
-                <Typography variant="h5" component="div">
-                    {entry.name ?? "Course Name"}
-                </Typography>
-                <Typography variant="body" component="div">
-                    {entry.description ?? "Course Description"}
-                </Typography>
-                <Typography variant="caption" component="div">
-                    CompletionRate: {entry.completionRate ?? 0}
-                </Typography>
-                <Button variant="contained" onClick={routeChange}>
-                    Learn More
-                </Button>
-            </CardContent>
+                margin: Constants.UI_HORIZ_OFFSET,
+                padding: Constants.UI_CORNER_RADIUS/4,
+                borderRadius: Constants.UI_CORNER_RADIUS
+            }}>
+            <Card>
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        {entry.name ?? "Course Name"}
+                    </Typography>
+                    <Typography variant="body" component="div">
+                        {entry.description ?? "Course Description"}
+                    </Typography>
+                    <Typography variant="caption" component="div">
+                        CompletionRate: {entry.completionRate ?? 0}
+                    </Typography>
+                    <Button variant="contained" onClick={routeChange}>
+                        Learn More
+                    </Button>
+                </CardContent>
         </Card>
+        </Grid>
     );
 }
--- front-end/src/pages/subjects/SubjectCard.jsx
+++ front-end/src/pages/subjects/SubjectCard.jsx
@@ -2,13 +2,15 @@ import React, {useState, useEffect} from 'react';
 import {useNavigate, useParams} from 'react-router-dom';
 import axios from 'axios';
 import Card from '@mui/material/Card';
-import {Button, CardContent, Typography} from "@mui/material";
+import {Button, CardContent, Grid, Typography, useTheme} from "@mui/material";
+import * as Constants from "../../util/Constants.mjs";
 
 export function SubjectCard(
     /** @type {{ entry: { id: number; name: string; description: string; completionRate: number; } }} */
     { entry }
 ) {
     const navigate = useNavigate();
+    const theme = useTheme();
     const routeChange = () => {
         navigate(`./subject_detail/${entry.id}`);
     };
@@ -19,19 +21,29 @@ export function SubjectCard(
         //     <p> {props.description??"a"}</p>
         //     <p> You have completed {props.completionRate??0} % </p>
         // </div>
-        <Card sx={{minWidth:200}}>
-            <CardContent>
-                <Typography variant='h5' component='div'>
-                    {entry.name??'Subject Name'}
-                </Typography>
-                <Typography variant='body' component='div'>
-                    {entry.description??"Subject Description"}
-                </Typography>
-                <Typography variant='subtitle' component='div'>
-                    CompletionRate: {entry.completionRate??0}
-                </Typography>
-                <Button variant='contained' onClick={routeChange}>Learn More</Button>
-            </CardContent>
-        </Card>
+        <Grid
+            sx={{
+                minWidth: 200,
+                margin: Constants.UI_HORIZ_OFFSET,
+                padding: Constants.UI_CORNER_RADIUS/4,
+                borderRadius: Constants.UI_CORNER_RADIUS
+            }}>
+            <Card>
+                <CardContent>
+                    <Typography variant='h5' component='div'>
+                        {entry.name??'Subject Name'}
+                    </Typography>
+                    <Typography variant='body' component='div'>
+                        {entry.description??"Subject Description"}
+                    </Typography>
+                    <Typography variant='subtitle' component='div'>
+                        CompletionRate: {entry.completionRate??0}
+                    </Typography>
+                    <Button variant='contained' onClick={routeChange}>
+                        Learn More
+                    </Button>
+                </CardContent>
+            </Card>
+        </Grid>
     );
 }

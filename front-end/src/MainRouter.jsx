15d2ce49d71e510a166f944d6690f7d9aeb21955
TY37Zhang <tz1308@nyu.edu>
2023-03-19 23:43:37 -0400

    <fix>: solve merge conflict in MainRouter
    <perf>: shorten import url in LandingUI.jsx

--- front-end/src/MainRouter.jsx
+++ front-end/src/MainRouter.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home/Home";
-import { Begin } from "./pages/landing/begin/Begin";
 import { Layout } from "./containers/Layout/Layout";
 import { SubjectList } from "./pages/subjects/SubjectList.jsx";
 import { SubjectDetail } from "./pages/subjects/SubjectDetail.jsx";
 import { RecentSubjectList} from "./pages/subjects/RecentSubjectList";
 import { CourseDetail } from "./pages/course/CourseDetail";
-import { SignUp } from "./pages/landing/signup/SignUp";
-import { SignIn } from "./pages/landing/signin/SignIn";
 import {SuggestSubjectList} from "./pages/subjects/SuggestSubjectList";
+import LandingPage from "./pages/landing/LandingPage";
+import EditProfile from "./pages/userprofile/EditProfile";
+import RecentlyUsedAvatars from "./pages/userprofile/RecentlyUsedAvatars";
 export const MainRouter = () => {
   // Currently, there is only one route to a default page:
   //      Level one routes should be included here
@@ -19,7 +19,6 @@ export const MainRouter = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/begin" element={<Begin />} />
           <Route path="/subjects" element={<SubjectList />} />
           <Route path="/subjects/subject_detail/:subjectId" element={<SubjectDetail />} />
           <Route path="/recent_subject_list" element={<RecentSubjectList />} />
@@ -27,8 +26,9 @@ export const MainRouter = () => {
           <Route path="/suggest_subject_list" element={<SuggestSubjectList />} />
           <Route path="/suggest_subject_list/subject_detail/:subjectId" element={<SubjectDetail />} />
           <Route path="/course_detail" element={<CourseDetail />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signin" element={<LandingPage />} />
+          <Route path="/edit_profile" element={<EditProfile />} />
+          <Route path="/recently_used_avatars" element={<RecentlyUsedAvatars />} />
         </Routes>
       </Layout>
     </div>

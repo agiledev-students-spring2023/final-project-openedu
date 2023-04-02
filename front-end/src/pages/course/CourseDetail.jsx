d4fb6f0627e50f20af0651da28bd692c4f0b94b4
TY37Zhang <tz1308@nyu.edu>
2023-04-02 13:32:48 -0400

    feat: SubjectDetail display course img, progress shown as a responsive bar

--- front-end/src/pages/course/CourseDetail.jsx
+++ front-end/src/pages/course/CourseDetail.jsx
@@ -1,130 +1,148 @@
-import {React,useState} from "react";
-import {Box, CardMedia, Paper, Typography, useTheme} from "@mui/material";
-import {CommentCard} from "./CommentCard";
+import { React, useState } from "react";
+import { Box, CardMedia, Paper, Typography, useTheme } from "@mui/material";
+import { CommentCard } from "./CommentCard";
 import * as Constants from "../../util/Constants.mjs";
 import * as Mockaroo from "../../mockApi/apis.mjs";
 import * as Logger from "../../util/Logger.mjs";
 
+export const CourseDetail = (props) => {
+  //TODO: Put useState here
+  const [courseInfo, setCourseInfo] = useState(undefined);
+  const [comments, setComments] = useState(undefined);
+  const [isLoaded, setLoaded] = useState(false);
+  const theme = useTheme();
 
-export const CourseDetail = props => {
+  Logger.verbose(CourseDetail.name + " Loaded!");
 
-    //TODO: Put useState here
-    const [courseInfo,setCourseInfo] = useState(undefined);
-    const [comments, setComments] = useState(undefined);
-    const [isLoaded, setLoaded] = useState(false);
-    const theme = useTheme();
+  useState(() => {
+    //TODO: Fetch actual data, use props.courseId
 
-    Logger.verbose(CourseDetail.name + " Loaded!");
+    setCourseInfo({
+      courseId: 0,
+      name: "foo",
+      detail: "ipsum_lorem",
+      language: "Java",
+      difficulty: "Hard",
+      url: "https://youtube.com",
+    });
 
-    useState(() => {
-        //TODO: Fetch actual data, use props.courseId
+    setComments([
+      {
+        userId: 0,
+        userName: "abc",
+        msg: "haha",
+      },
+      {
+        userId: 1,
+        userName: "abcd",
+        msg: "hahahahaha",
+      },
+    ]);
 
-        setCourseInfo({
-            courseId: 0,
-            name: "foo",
-            detail: "ipsum_lorem",
-            language: "Java",
-            difficulty: "Hard",
-            url: "https://youtube.com",
-        });
+    setLoaded(true);
+  }, []);
 
-        setComments([
-            {
-                userId : 0,
-                userName : "abc",
-                msg: "haha"
-            },
-            {
-                userId : 1,
-                userName : "abcd",
-                msg: "hahahahaha"
-            }
-        ]);
+  //const imgUrl = Mockaroo.mockImageApi(1920,1080);
 
-        setLoaded(true);
+  //Logger.info(`Image URL: ${imgUrl}`);
 
-    },[]);
+  return (
+    <>
+      {isLoaded ? (
+        <div>
+          <Box
+            sx={{
+              margin: Constants.UI_HORIZ_OFFSET,
+            }}
+          >
+            {/*Course Image*/}
+            <CardMedia
+              alt="course_image"
+              image={Mockaroo.mockImageApi(1920, 1080)}
+              sx={{
+                height: 300,
+                borderRadius: Constants.UI_CORNER_RADIUS,
+              }}
+            />
 
-    //const imgUrl = Mockaroo.mockImageApi(1920,1080);
+            {/* Course Title and Description Section */}
+            <Box
+              sx={{
+                marginY: 3,
+              }}
+            >
+              {/*Course Name*/}
+              <Typography
+                variant="h1"
+                sx={{
+                  //margin: Constants.UI_HORIZ_OFFSET,
+                  textAlign: "left",
+                  fontWeight: "bold",
+                  //fontFamily: 'Roboto',
+                  letterSpacing: 2,
+                  //color: "text.primary",
+                }}
+              >
+                {courseInfo.name}
+              </Typography>
 
-    //Logger.info(`Image URL: ${imgUrl}`);
-
-    return <>
-
-        {isLoaded ? <div>
-
-            <Box sx={{
-                margin: Constants.UI_HORIZ_OFFSET
-            }}>
-
-                {/*Course Image*/}
-                <CardMedia
-                    alt="course_image"
-                    image={Mockaroo.mockImageApi(1920,1080)}
-                    sx={{
-                        height:300,
-                        borderRadius: Constants.UI_CORNER_RADIUS
-                    }}
-                />
-
-
-                {/* Course Title and Description Section */}
-                <Box sx={{
-                    marginY: 3
-                }}>
-                    {/*Course Name*/}
-                    <Typography variant="h1" sx={{
-                        //margin: Constants.UI_HORIZ_OFFSET,
-                        textAlign: 'left',
-                        fontWeight: 'bold',
-                        //fontFamily: 'Roboto',
-                        letterSpacing: 2
-                        //color: "text.primary",
-                    }}>{courseInfo.name}</Typography>
-
-                    {/*Course Description*/}
-                    <Typography variant="h3" sx={{
-                        //fontFamily: 'Roboto',
-                        textAlign: 'left'
-                    }}>{courseInfo.detail}
-                    </Typography>
-                </Box>
-
-                {/* Course Information Section */}
-                <Paper
-
-                    elevation={0}
-                    sx={{
-                        boxShadow : 1,
-                        marginY: 3,
-                        borderRadius: Constants.UI_CORNER_RADIUS,
-                        padding: Constants.UI_CORNER_RADIUS / 2
-                    }}
-                >
-                    <Typography variant="h5" sx={{
-                        textAlign: 'left',
-                        fontFamily: "sans-serif"
-                    }}>Language: {courseInfo.language}</Typography>
-
-
-                    <Typography variant="h5" sx={{
-                        textAlign: 'left'
-                    }}>Difficulty: {courseInfo.difficulty}</Typography>
-
-                    <a href={courseInfo.url}></a>
-                </Paper>
-
-                {/*//<Divider/>*/}
-                {
-                    comments.map(element => (<CommentCard key={element.userId} userName={element.userName} msg={element.msg}/>)
-                    )
-                }
-
-
+              {/*Course Description*/}
+              <Typography
+                variant="h3"
+                sx={{
+                  //fontFamily: 'Roboto',
+                  textAlign: "left",
+                }}
+              >
+                {courseInfo.detail}
+              </Typography>
             </Box>
 
+            {/* Course Information Section */}
+            <Paper
+              elevation={0}
+              sx={{
+                boxShadow: 1,
+                marginY: 3,
+                borderRadius: Constants.UI_CORNER_RADIUS,
+                padding: Constants.UI_CORNER_RADIUS / 2,
+              }}
+            >
+              <Typography
+                variant="h5"
+                sx={{
+                  textAlign: "left",
+                  fontFamily: "sans-serif",
+                }}
+              >
+                Language: {courseInfo.language}
+              </Typography>
 
-        </div> : <h1>Fetching data...</h1>}
+              <Typography
+                variant="h5"
+                sx={{
+                  textAlign: "left",
+                }}
+              >
+                Difficulty: {courseInfo.difficulty}
+              </Typography>
 
-    </>;
+              <a href={courseInfo.url}></a>
+            </Paper>
+
+            {/*//<Divider/>*/}
+            {comments.map((element) => (
+              <CommentCard
+                key={element.userId}
+                userName={element.userName}
+                msg={element.msg}
+              />
+            ))}
+          </Box>
+        </div>
+      ) : (
+        <h1>Fetching data...</h1>
+      )}
+    </>
+  );
 };
--- front-end/src/pages/subjects/CourseCard.jsx
+++ front-end/src/pages/subjects/CourseCard.jsx
@@ -1,42 +1,103 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {Button, Card, Grid,CardContent, Typography} from "@mui/material";
+import {
+  Button,
+  Card,
+  Grid,
+  CardContent,
+  Typography,
+  Box,
+  CardMedia,
+  LinearProgress,
+} from "@mui/material";
 import * as Constants from "../../util/Constants.mjs";
+import CircularProgress from "@mui/material/CircularProgress";
+import * as PropTypes from "prop-types";
+import * as Mockaroo from "../../mockApi/apis.mjs";
 
+//need fix during Sprint3,
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+LinearProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate and buffer variants.
+   * Value between 0 and 100.
+   */
+  value: PropTypes.number.isRequired,
+};
 
 export function CourseCard(
-    /** @type {{ entry: { id: number; name: string; description: string; completionRate: number; } }} */
-    { entry }
+  /** @type {{ entry: { id: number; name: string; description: string; completionRate: number; } }} */
+  { entry }
 ) {
-    const navigate = useNavigate();
-    const routeChange = () => {
-        navigate(`/courses/detail/${entry.id}`);
-    };
-    return (
-        <Grid
+  const navigate = useNavigate();
+  const routeChange = () => {
+    navigate(`/courses/detail/${entry.id}`);
+  };
+  return (
+    <Grid
+      sx={{
+        // minWidth: 200,
+        // margin: Constants.UI_HORIZ_OFFSET,
+        padding: Constants.UI_CORNER_RADIUS / 4,
+        borderRadius: Constants.UI_CORNER_RADIUS * 2,
+        marginBottom: 2,
+      }}
+    >
+      <Card>
+        <CardContent>
+          <CardMedia
             sx={{
-                // minWidth: 200,
-                // margin: Constants.UI_HORIZ_OFFSET,
-                // padding: Constants.UI_CORNER_RADIUS/4,
-                borderRadius: Constants.UI_CORNER_RADIUS,
-                marginBottom: 2
-            }}>
-            <Card>
-                <CardContent>
-                    <Typography variant="h5" component="div">
-                        {entry.name ?? "Course Name"}
-                    </Typography>
-                    <Typography variant="body" component="div">
-                        {entry.description ?? "Course Description"}
-                    </Typography>
-                    <Typography variant="caption" component="div">
-                        CompletionRate: {entry.completionRate ?? 0}
-                    </Typography>
-                    <Button variant="contained" onClick={routeChange}>
-                        Learn More
-                    </Button>
-                </CardContent>
-        </Card>
-        </Grid>
-    );
+              borderRadius: Constants.UI_HORIZ_OFFSET,
+            }}
+            component="img"
+            height="140"
+            image={Mockaroo.mockImageApi(1920, 1080)}
+            alt="green iguana"
+          />
+          <Typography variant="h5" component="div">
+            {entry.name ?? "Course Name"}
+          </Typography>
+          <Typography variant="body" component="div">
+            {entry.description ?? "Course Description"}
+          </Typography>
+          {/*<Typography variant="caption" component="div">*/}
+          {/*  CompletionRate: {entry.completionRate ?? 0}*/}
+          {/*</Typography>*/}
+          <Box
+            sx={{
+              display: "Grid",
+            }}
+          >
+            <Box
+              sx={{
+                width: "100%",
+              }}
+            >
+              <LinearProgressWithLabel
+                value={entry.completionRate ?? 0}
+                component="div"
+              />
+            </Box>
+            <Button variant="contained" component="div" onClick={routeChange}>
+              Continue
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 }

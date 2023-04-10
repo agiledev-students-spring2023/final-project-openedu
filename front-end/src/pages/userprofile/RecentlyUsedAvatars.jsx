94b6bababedf49ff480c6a18618384b3c7386d69
Hoooao <hao021014@163.com>
2023-04-10 10:58:26 -0400

    chore: backend api for edit profile

--- front-end/src/pages/userprofile/RecentlyUsedAvatars.jsx
+++ front-end/src/pages/userprofile/RecentlyUsedAvatars.jsx
@@ -2,31 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { Avatar, Typography, Box, Button } from '@mui/material';
 import { mockImageApi } from '../../mockApi/apis.mjs';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import * as Util from "../../util/Util.mjs";
 
 
 // Chosen avatar effect is considered
 export default function RecentlyUsedAvatars() {
     const [avatars, setAvatars] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
     const [selectedAvatar, setSelectedAvatar] = useState(null);
     const navigate = useNavigate();
+    // Mocked!!!!
+    const tmpAvs = [1, 2, 3, 4, 5, 6];
     useEffect(() => {
-        // axios({
-        //     method: "GET",
-        //     //url: `${baseURL}/api/user/profile TBD!`
-        // }).then(res => {
-        //     const user = res.data
-        // }).catch(err => {
-        //     console.log(err)
-        // })
-        console.log("RecentlyUsedAvatars Axios TBD");
+        axios
+            .get(Util.getServerAddr() + `/profile/info?token=123`)
+            .then((response) => {
+                const userInfo = response.data["content"];
+                setAvatars(userInfo["avatar"]);
+                // Mocking !!
+                // for (let index = 0; index < 12; index++) {
+                //     tmpAvs.push(avatars);
+                // }
+                setIsLoaded(true);
+            })
+            .catch((error) => console.error(error));
     }, []);
 
 
-    // Mocking Avatars
-    const tmpAvs = [];
-    for (let index = 0; index < 12; index++) {
-        tmpAvs.push(mockImageApi(200));
-    }
+
 
     const handleSaveChanges = () => {
         console.log("RecentlyUsedAvatars/handleSaveChanges Mocked");
@@ -49,46 +53,49 @@ export default function RecentlyUsedAvatars() {
 
 
     return (
-        <Box>
-            <Typography variant='h3' sx={{ marginTop: '12%' }}>Choose One</Typography>
+        <>
+            {!isLoaded ? (<Box></Box>) : (<Box>
+                <Typography variant='h3' sx={{ marginTop: '12%' }}>Choose One</Typography>
 
-            <Box sx={{
-                marginTop: '15%',
-                display: 'flex',
-                flexDirection: 'row',
-                flexWrap: 'wrap',
-                justifyContent: 'space-evenly',
-                marginLeft: '5%',
-                marginBottom: '7%',
-            }}>
-                {tmpAvs.map((ele, id) => {
-                    return (
-                        <Avatar
-                            src={ele}
-                            key={id}
-                            sx={{
-                                width: '80px',
-                                height: '80px',
-                                display: 'flex',
-                                marginRight: '5%',
-                                marginBottom: '5%'
-                            }}
-                            onClick={handleAvatarClick} />
-                    );
-                })}
-            </Box>
-            <Button
-                variant='contained'
-                sx={{
-                    position: 'absolute',
-                    bottom: '17%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: "75%",
-                    borderRadius: 2,
-                }}
-                onClick={handleSaveChanges}>
-                Save Changes </Button>
-        </Box>
+                <Box sx={{
+                    marginTop: '15%',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    justifyContent: 'space-evenly',
+                    marginLeft: '5%',
+                    marginBottom: '7%',
+                }}>
+                    {tmpAvs.map((ele, id) => {
+                        return (
+                            <Avatar
+                                src={avatars}
+                                key={id}
+                                sx={{
+                                    width: '80px',
+                                    height: '80px',
+                                    display: 'flex',
+                                    marginRight: '5%',
+                                    marginBottom: '5%'
+                                }}
+                                onClick={handleAvatarClick} />
+                        );
+                    })}
+                </Box>
+                <Button
+                    variant='contained'
+                    sx={{
+                        position: 'absolute',
+                        bottom: '17%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: "75%",
+                        borderRadius: 2,
+                    }}
+                    onClick={handleSaveChanges}>
+                    Save Changes </Button>
+            </Box>)}
+        </>
+
     );
 }

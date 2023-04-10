cadb2749df7c38b8bee6a95561a815bd5c37688b
lapisliu <lxteng2016@126.com>
2023-04-10 11:46:41 -0400

    chore:pull avatar img from backend in edit-profile and recently-used-avatars

--- front-end/src/pages/userprofile/RecentlyUsedAvatars.jsx
+++ front-end/src/pages/userprofile/RecentlyUsedAvatars.jsx
@@ -1,94 +1,110 @@
-import React, { useEffect, useState } from 'react';
-import { Avatar, Typography, Box, Button } from '@mui/material';
-import { mockImageApi } from '../../mockApi/apis.mjs';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { Avatar, Typography, Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import * as Util from "../../util/Util.mjs";
 
 // Chosen avatar effect is considered
 export default function RecentlyUsedAvatars() {
-    const [avatars, setAvatars] = useState([]);
-    const [selectedAvatar, setSelectedAvatar] = useState(null);
-    const navigate = useNavigate();
-    useEffect(() => {
-        // axios({
-        //     method: "GET",
-        //     //url: `${baseURL}/api/user/profile TBD!`
-        // }).then(res => {
-        //     const user = res.data
-        // }).catch(err => {
-        //     console.log(err)
-        // })
-        console.log("RecentlyUsedAvatars Axios TBD");
-    }, []);
+  const [avatars, setAvatars] = useState([]);
+  const [selectedAvatar, setSelectedAvatar] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const navigate = useNavigate();
+  // useEffect(() => {
+  //     // axios({
+  //     //     method: "GET",
+  //     //     //url: `${baseURL}/api/user/profile TBD!`
+  //     // }).then(res => {
+  //     //     const user = res.data
+  //     // }).catch(err => {
+  //     //     console.log(err)
+  //     // })
+  //     console.log("RecentlyUsedAvatars Axios TBD");
+  // }, []);
 
+  useEffect(() => {
+    axios
+      .get(Util.getServerAddr() + "/background-image", {
+        params: { token: "1234", width: "200", height: "200" },
+      })
+      .then((response) => {
+        setImageUrl(response.data["content"]);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
-    // Mocking Avatars
-    const tmpAvs = [];
-    for (let index = 0; index < 12; index++) {
-        tmpAvs.push(mockImageApi(200));
-    }
+  const tmpAvs = [];
+  for (let index = 0; index < 12; index++) {
+    tmpAvs.push(imageUrl);
+  }
 
-    const handleSaveChanges = () => {
-        console.log("RecentlyUsedAvatars/handleSaveChanges Mocked");
-        navigate(-1);
-        //TODO: Enable this in Sprint 2
-        // axios({
-        //     method: "POST",
-        //     url: `${baseURL}/api/user/update_profile`,
-        //     data: {
-        //         selectedAvatar
-        //     }
-        // }).then(res => {
-        //     navigate(-1)
-        // }).catch(err=>{console.log(err)})
-    };
+  const handleSaveChanges = () => {
+    console.log("RecentlyUsedAvatars/handleSaveChanges Mocked");
+    navigate(-1);
+    //TODO: Enable this in Sprint 2
+    // axios({
+    //     method: "POST",
+    //     url: `${baseURL}/api/user/update_profile`,
+    //     data: {
+    //         selectedAvatar
+    //     }
+    // }).then(res => {
+    //     navigate(-1)
+    // }).catch(err=>{console.log(err)})
+  };
 
-    const handleAvatarClick = (item) => {
-        setSelectedAvatar(item);
-    };
+  const handleAvatarClick = (item) => {
+    setSelectedAvatar(item);
+  };
 
+  return (
+    <Box>
+      <Typography variant="h3" sx={{ marginTop: "12%" }}>
+        Choose One
+      </Typography>
 
-    return (
-        <Box>
-            <Typography variant='h3' sx={{ marginTop: '12%' }}>Choose One</Typography>
-
-            <Box sx={{
-                marginTop: '15%',
-                display: 'flex',
-                flexDirection: 'row',
-                flexWrap: 'wrap',
-                justifyContent: 'space-evenly',
-                marginLeft: '5%',
-                marginBottom: '7%',
-            }}>
-                {tmpAvs.map((ele, id) => {
-                    return (
-                        <Avatar
-                            src={ele}
-                            key={id}
-                            sx={{
-                                width: '80px',
-                                height: '80px',
-                                display: 'flex',
-                                marginRight: '5%',
-                                marginBottom: '5%'
-                            }}
-                            onClick={handleAvatarClick} />
-                    );
-                })}
-            </Box>
-            <Button
-                variant='contained'
-                sx={{
-                    position: 'absolute',
-                    bottom: '17%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: "75%",
-                    borderRadius: 2,
-                }}
-                onClick={handleSaveChanges}>
-                Save Changes </Button>
-        </Box>
-    );
+      <Box
+        sx={{
+          marginTop: "15%",
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-evenly",
+          marginLeft: "5%",
+          marginBottom: "7%",
+        }}
+      >
+        {tmpAvs.map((ele, id) => {
+          return (
+            <Avatar
+              src={ele}
+              key={id}
+              sx={{
+                width: "80px",
+                height: "80px",
+                display: "flex",
+                marginRight: "5%",
+                marginBottom: "5%",
+              }}
+              onClick={handleAvatarClick}
+            />
+          );
+        })}
+      </Box>
+      <Button
+        variant="contained"
+        sx={{
+          position: "absolute",
+          bottom: "17%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "75%",
+          borderRadius: 2,
+        }}
+        onClick={handleSaveChanges}
+      >
+        Save Changes{" "}
+      </Button>
+    </Box>
+  );
 }

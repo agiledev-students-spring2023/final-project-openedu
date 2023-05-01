09750b9f0aad0e2f093588777bf60e36ef2e7c36
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2023-05-01 14:47:03 -0400

    fix: UI course card fix (WIP)

--- front-end/src/containers/CourseCard/CourseCardAtHome.jsx
+++ front-end/src/containers/CourseCard/CourseCardAtHome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Paper, Typography } from "@mui/material";
+import {Backdrop, Box, Paper, Typography} from "@mui/material";
 
 export default function CourseCardAtHome({ entry }) {
   const { name, completionRate } = entry.name
@@ -11,57 +11,56 @@ export default function CourseCardAtHome({ entry }) {
 
 
   return (
+  <>
     <Paper
       variant="outlined"
       elevation={0}
       square
 
       sx={{
+        display: "flex",
         width: cardSize,
         height: cardSize,
         borderRadius: "20px",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundImage: `url(${entry.imageUrl ?? "Course Image"})`,
+          // filter: "blur(3px)"
       }}
 
-    >
-      <Box
-
+    />
+    <Box
         sx={{
           display: "flex",
           flexDirection: "column",
-          marginTop: "50%",
-          marginLeft: "10%",
+          marginTop: "10%",
+          marginLeft: "5%",
         }}
 
-      >
-        <Typography
+    >
+    <Typography
           variant="h6"
           sx={{
             display: "flex",
             fontSize: "15px",
-              fontWeight: "bold",
-              textTransform: "capitalize"
+            fontWeight: "bold",
+            textTransform: "capitalize",
+            color: "white",
           }}
-        >
-
-          {name}
-
-        </Typography>
-        <Typography
-          sx={{
-            display: "flex",
-            fontSize: "15px",
-            fontWeight: "medium",
-              letterSpacing: 1,
-          }}
-        >
-
-          {completionRate}
-
-        </Typography>
-      </Box>
-    </Paper>
+    >
+      {name}
+    </Typography>
+    <Typography
+      sx={{
+        display: "flex",
+        fontSize: "15px",
+        fontWeight: "medium",
+          letterSpacing: 1,
+      }}
+    >
+      {completionRate}
+    </Typography>
+    </Box>
+    </>
   );
 }

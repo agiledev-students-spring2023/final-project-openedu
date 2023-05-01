b9e9a4996b81f216819761fab8ae40f7b7790831
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2023-05-01 15:28:10 -0400

    style: Home Page Course Card UI Fix fix: Remove Completion rate as its not pulling info

--- front-end/src/containers/CourseCard/CourseCardAtHome.jsx
+++ front-end/src/containers/CourseCard/CourseCardAtHome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Backdrop, Box, Paper, Typography} from "@mui/material";
+import {Box, Paper, Typography} from "@mui/material";
 
 export default function CourseCardAtHome({ entry }) {
   const { name, completionRate } = entry.name
@@ -13,54 +13,66 @@ export default function CourseCardAtHome({ entry }) {
   return (
   <>
     <Paper
-      variant="outlined"
-      elevation={0}
-      square
+        variant="outlined"
+        elevation={0}
+        square
 
-      sx={{
-        display: "flex",
-        width: cardSize,
-        height: cardSize,
-        borderRadius: "20px",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundImage: `url(${entry.imageUrl ?? "Course Image"})`,
-          // filter: "blur(3px)"
-      }}
-
-    />
-    <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          marginTop: "10%",
-          marginLeft: "5%",
+            width: cardSize,
+            height: cardSize,
+            borderRadius: "20px",
+            display: 'grid',
+            gridTemplateColumn: '1fr',
+            gridTemplateRows: '1fr',
+    
+            // backgroundSize: "cover",
+            // backgroundRepeat: "no-repeat",
+            // backgroundImage: `url(${entry.imageUrl ?? "Course Image"})`,
+            // filter: "blur(2px)"
         }}
-
     >
-    <Typography
-          variant="h6"
+        <Box
+          
           sx={{
             display: "flex",
-            fontSize: "15px",
-            fontWeight: "bold",
-            textTransform: "capitalize",
-            color: "white",
+            width: cardSize,
+            height: cardSize,
+            borderRadius: "20px",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+              gridColumn: '1 / 1',
+              gridRow: '1 / 1',
+            backgroundImage: `url(${entry.imageUrl ?? "Course Image"})`,
+            filter: "blur(2px) brightness(0.5)",
           }}
-    >
-      {name}
-    </Typography>
-    <Typography
-      sx={{
-        display: "flex",
-        fontSize: "15px",
-        fontWeight: "medium",
-          letterSpacing: 1,
-      }}
-    >
-      {completionRate}
-    </Typography>
-    </Box>
+    
+        />
+        <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              marginTop: "10%",
+              marginLeft: "5%",
+                gridColumn: '1 / 1',
+                gridRow: '1 / 1',
+                zIndex:1000,
+            }}
+        >
+        
+            <Typography
+                  variant="h6"
+                  sx={{
+                    display: "flex",
+                    fontSize: "15px",
+                    fontWeight: "bold",
+                    textTransform: "capitalize",
+                    color: "white",
+                  }}
+            >
+              {name}
+            </Typography>
+        </Box>
+    </Paper>
     </>
   );
 }

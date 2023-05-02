d20c517691f34f777c59c17fc5de95f44824f24e
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2023-05-01 22:05:24 -0400

    chore: code style refactor

--- front-end/src/pages/subjects/SubjectCard.jsx
+++ front-end/src/pages/subjects/SubjectCard.jsx
@@ -1,140 +1,140 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
-  Card,
-  Collapse,
-  Box,
-  Button,
-  CardContent,
-  CardMedia,
-  Grid,
-  LinearProgress,
-  Typography,
-  useTheme,
+    Card,
+    Collapse,
+    Box,
+    Button,
+    CardContent,
+    CardMedia,
+    Grid,
+    LinearProgress,
+    Typography,
+    useTheme,
 } from "@mui/material";
 import * as Constants from "../../util/Constants.mjs";
 import * as PropTypes from "prop-types";
 
 function LinearProgressWithLabel(props) {
-  return (
-    <Box sx={{ display: "flex", alignItems: "center" }}>
-      <Box sx={{ width: "100%", mr: 1 }}>
-        <LinearProgress variant="determinate" {...props} />
-      </Box>
-      <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2" color="text.secondary">{`${Math.round(
-          props.value
-        )}%`}</Typography>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Box sx={{ width: "100%", mr: 1 }}>
+                <LinearProgress variant="determinate" {...props} />
+            </Box>
+            <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="text.secondary">{`${Math.round(
+                    props.value
+                )}%`}</Typography>
+            </Box>
+        </Box>
+    );
 }
 
 LinearProgressWithLabel.propTypes = {
-  /**
-   * The value of the progress indicator for the determinate and buffer variants.
-   * Value between 0 and 100.
-   */
-  value: PropTypes.number.isRequired,
+    /**
+     * The value of the progress indicator for the determinate and buffer variants.
+     * Value between 0 and 100.
+     */
+    value: PropTypes.number.isRequired,
 };
 export function SubjectCard(
-  /** @type {{ entry: { id: number; name: string; description: string; completionRate: number; } }} */
-  { entry }
+    /** @type {{ entry: { id: number; name: string; description: string; completionRate: number; } }} */
+    { entry }
 ) {
-  const navigate = useNavigate();
-  const theme = useTheme();
-  const routeChange = () => {
-    navigate(`../detail/${entry.subjectId}`);
-  };
-  const [isExpanded, setIsExpanded] = useState(false);
+    const navigate = useNavigate();
+    const theme = useTheme();
+    const routeChange = () => {
+        navigate(`../detail/${entry.subjectId}`);
+    };
+    const [isExpanded, setIsExpanded] = useState(false);
 
-  const handleExpandClick = () => {
-    setIsExpanded(!isExpanded);
-  };
+    const handleExpandClick = () => {
+        setIsExpanded(!isExpanded);
+    };
 
-  return (
-    <Grid
-      sx={{
-        padding: Constants.UI_CORNER_RADIUS / 4,
-        borderRadius: Constants.UI_CORNER_RADIUS * 8,
-        marginBottom: 2,
-      }}
-    >
-      <Card
-        sx={{
-          marginBottom: 2,
-        }}
-      >
-        <CardContent>
-          <CardMedia
+    return (
+        <Grid
             sx={{
-              borderRadius: Constants.UI_HORIZ_OFFSET / 2,
+                padding: Constants.UI_CORNER_RADIUS / 4,
+                borderRadius: Constants.UI_CORNER_RADIUS * 8,
+                marginBottom: 2,
             }}
-            id={entry.subjectId ?? 0}
-            component="img"
-            height="140"
-            image={entry.imageUrl ?? "Subject Image"}
-          />
-          <Typography
-            variant="h5"
-            component="div"
-            sx={{
-              marginTop: "1vh",
-              fontWeight: "semi-bold",
-            }}
-          >
-            {entry.name ?? "Subject Name"}
-          </Typography>
-
-          <Collapse in={isExpanded} timeout="auto" collapsedSize={70}>
-            <Typography
-              variant="body"
-              color="text.secondary"
-              align="left"
-              sx={{
-                display: "flex",
-                marginTop: "4px",
-              }}
+        >
+            <Card
+                sx={{
+                    marginBottom: 2,
+                }}
             >
-              {entry.description}
-            </Typography>
-          </Collapse>
-          <Button
-            variant="outlined"
-            onClick={handleExpandClick}
-            size="small"
-            sx={{
-              position: "relative",
-              right: "-35%",
-              color: "white",
-              marginTop: "1vh",
-              backgroundColor: theme.palette.neutral.main,
-            }}
-          >
-            {isExpanded ? "Collapse" : "Read more"}
-          </Button>
+                <CardContent>
+                    <CardMedia
+                        sx={{
+                            borderRadius: Constants.UI_HORIZ_OFFSET / 2,
+                        }}
+                        id={entry.subjectId ?? 0}
+                        component="img"
+                        height="140"
+                        image={entry.imageUrl ?? "Subject Image"}
+                    />
+                    <Typography
+                        variant="h5"
+                        component="div"
+                        sx={{
+                            marginTop: "1vh",
+                            fontWeight: "semi-bold",
+                        }}
+                    >
+                        {entry.name ?? "Subject Name"}
+                    </Typography>
 
-          <Box
-            sx={{
-              display: "Grid",
-            }}
-          >
-            <Box
-              sx={{
-                width: "100%",
-              }}
-            >
-              <LinearProgressWithLabel
-                value={entry.completionRate ?? 0}
-                component="div"
-              />
-            </Box>
-            <Button variant="contained" onClick={routeChange}>
-              Learn More
-            </Button>
-          </Box>
-        </CardContent>
-      </Card>
-    </Grid>
-  );
+                    <Collapse in={isExpanded} timeout="auto" collapsedSize={70}>
+                        <Typography
+                            variant="body"
+                            color="text.secondary"
+                            align="left"
+                            sx={{
+                                display: "flex",
+                                marginTop: "4px",
+                            }}
+                        >
+                            {entry.description}
+                        </Typography>
+                    </Collapse>
+                    <Button
+                        variant="outlined"
+                        onClick={handleExpandClick}
+                        size="small"
+                        sx={{
+                            position: "relative",
+                            right: "-35%",
+                            color: "white",
+                            marginTop: "1vh",
+                            backgroundColor: theme.palette.neutral.main,
+                        }}
+                    >
+                        {isExpanded ? "Collapse" : "Read more"}
+                    </Button>
+
+                    <Box
+                        sx={{
+                            display: "Grid",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                width: "100%",
+                            }}
+                        >
+                            <LinearProgressWithLabel
+                                value={entry.completionRate ?? 0}
+                                component="div"
+                            />
+                        </Box>
+                        <Button variant="contained" onClick={routeChange}>
+                            Learn More
+                        </Button>
+                    </Box>
+                </CardContent>
+            </Card>
+        </Grid>
+    );
 }
--- front-end/src/pages/subjects/SuggestSubjectList.jsx
+++ front-end/src/pages/subjects/SuggestSubjectList.jsx
@@ -8,96 +8,96 @@ import * as Logger from "../../util/Logger.mjs";
 import * as Util from "../../util/Util.mjs";
 
 export function SuggestSubjectList() {
-  const url = Util.getServerAddr() + "/subject/recommend";
+    const url = Util.getServerAddr() + "/subject/recommend";
 
-  const [data, setData] = useState([]);
-  const [isLoaded, setLoaded] = useState(false);
+    const [data, setData] = useState([]);
+    const [isLoaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-    Util.invokeCallback("setNewPage", 1);
+    useEffect(() => {
+        Util.invokeCallback("setNewPage", 1);
 
-    console.log("fetching subject information");
-    axios
-      .get(url, {
-        params: {
-          token: Util.readLocalValue("token") ?? 12345,
-          mock: "false",
-        },
-      })
-      .then((response) => {
-        Logger.info(
-          `SubjectList's axios got the following data: \n ${response.data}`
-        );
-        setData(response.data["content"]);
-        console.log(response.data);
-        setLoaded(true);
-      })
-      .catch((err) => {
-        console.log("error fetching subject information");
-        console.log(err);
-        Logger.error("error fetching subject information");
-        Logger.error(err);
+        console.log("fetching subject information");
+        axios
+            .get(url, {
+                params: {
+                    token: Util.readLocalValue("token") ?? 12345,
+                    mock: "false",
+                },
+            })
+            .then((response) => {
+                Logger.info(
+                    `SubjectList's axios got the following data: \n ${response.data}`
+                );
+                setData(response.data["content"]);
+                console.log(response.data);
+                setLoaded(true);
+            })
+            .catch((err) => {
+                console.log("error fetching subject information");
+                console.log(err);
+                Logger.error("error fetching subject information");
+                Logger.error(err);
 
-        setData([
-          {
-            id: 3,
-            name: "backupSuggestedSubject",
-            description: "backupSuggestedDescription",
-            completionRate: 37,
-          },
-        ]);
+                setData([
+                    {
+                        id: 3,
+                        name: "backupSuggestedSubject",
+                        description: "backupSuggestedDescription",
+                        completionRate: 37,
+                    },
+                ]);
 
-        setLoaded(true);
-      });
-  }, []);
+                setLoaded(true);
+            });
+    }, []);
 
-  return (
-    <Box
-      sx={{
-        marginX: Constants.UI_HORIZ_OFFSET,
-      }}
-    >
-      <Typography
-        variant="h4"
-        sx={{
-          marginTop: 1,
-          fontWeight: "bold",
-        }}
-      >
-        You May Like
-      </Typography>
-      {/*<Typography variant="h6">What would you like to learn today?</Typography>*/}
+    return (
+        <Box
+            sx={{
+                marginX: Constants.UI_HORIZ_OFFSET,
+            }}
+        >
+            <Typography
+                variant="h4"
+                sx={{
+                    marginTop: 1,
+                    fontWeight: "bold",
+                }}
+            >
+                You May Like
+            </Typography>
+            {/*<Typography variant="h6">What would you like to learn today?</Typography>*/}
 
-      <Box
-        sx={{
-          marginBottom: 3,
-        }}
-      >
-        <TypeAnimation
-          sequence={[
-            "What would you like to learn today?", // Types 'One'
-            1000, // Waits 1s
-            "Pick one that interests you!",
-            2000,
-            "Pick a Subject, take a look at it!",
-            3000,
+            <Box
+                sx={{
+                    marginBottom: 3,
+                }}
+            >
+                <TypeAnimation
+                    sequence={[
+                        "What would you like to learn today?", // Types 'One'
+                        1000, // Waits 1s
+                        "Pick one that interests you!",
+                        2000,
+                        "Pick a Subject, take a look at it!",
+                        3000,
 
-            async () => {
-              Logger.verbose("SubjectList: Typewriter Sequence completed"); // Place optional callbacks anywhere in the array
-            },
-          ]}
-          wrapper="span"
-          cursor={true}
-          repeat={1}
-          sx={{ fontSize: "1em", display: "inline-block" }}
-        />
-      </Box>
+                        async () => {
+                            Logger.verbose("SubjectList: Typewriter Sequence completed"); // Place optional callbacks anywhere in the array
+                        },
+                    ]}
+                    wrapper="span"
+                    cursor={true}
+                    repeat={1}
+                    sx={{ fontSize: "1em", display: "inline-block" }}
+                />
+            </Box>
 
-      {isLoaded ? (
-        data.map((entry) => <SubjectCard key={entry.subjectId} entry={entry} />)
-      ) : (
-        <div />
-      )}
-    </Box>
-  );
+            {isLoaded ? (
+                data.map((entry) => <SubjectCard key={entry.subjectId} entry={entry} />)
+            ) : (
+                <div />
+            )}
+        </Box>
+    );
 }
